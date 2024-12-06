5488783145e99c7827a84805f11b2fb567c12dec
James Esguerra <jamesesguerra025@gmail.com>
2024-12-06 11:37:17 +0800

    add support for truncating prices in billions

--- src/app/pipes/price-truncate.pipe.ts
+++ src/app/pipes/price-truncate.pipe.ts
@@ -5,7 +5,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class PriceTruncatePipe implements PipeTransform {
   transform(value: number): string {
-    if (value >= 1_000_000) {
+    if (value >= 1_000_000_000) {
+      return `${(value / 1_000_000_000).toFixed(1).replace(/\.0$/, '')}B`;
+    } else if (value >= 1_000_000) {
       return `${(value / 1_000_000).toFixed(1).replace(/\.0$/, '')}M`;
     } else if (value >= 1_000) {
       return `${(value / 1_000).toFixed(1).replace(/\.0$/, '')}K`;
