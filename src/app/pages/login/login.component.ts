bc7ab567a9d101604a81665908aba998c442218f
James Esguerra <jamesesguerra025@gmail.com>
2024-12-09 10:54:50 +0800

    add spinner to login button

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { LayoutService } from 'src/app/layout/service/app.layout.service';
 import { ToastService } from 'src/app/layout/service/toast.service';
 import { UserService } from 'src/app/services/user.service';
@@ -12,7 +13,7 @@ import { UserService } from 'src/app/services/user.service';
 export class LoginComponent {
     username!: string;
     password!: string;
-    errorMessage: string;
+    showErrorMessage = false;
 
     constructor(
         public layoutService: LayoutService,
@@ -20,12 +21,23 @@ export class LoginComponent {
         private router: Router
     )
     { 
+        this.isLoadingSubject = new BehaviorSubject<boolean>(false);
+        this.isLoading$ = this.isLoadingSubject.asObservable();
+
         if (this.userService.currentUser()) this.router.navigate(['/']);
     }
 
+    private isLoadingSubject: BehaviorSubject<boolean>;
+    isLoading$: Observable<boolean>;
+
     onLogin() {
+        this.isLoadingSubject.next(true);
+
         this.userService.login({ username: this.username, password: this.password }).subscribe({
-            error: () => this.errorMessage = "Invalid username or password"
+            error: () => {
+                this.isLoadingSubject.next(false);
+                this.showErrorMessage = true;
+            }
         });
     }
 }
