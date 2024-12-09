046f9fa4764040e54eb4d8397731446e62843200
James Esguerra <jamesesguerra025@gmail.com>
2024-12-09 17:08:54 +0800

    add image upload to add watch record

--- src/app/pages/sheets/add-watch/add-watch.component.ts
+++ src/app/pages/sheets/add-watch/add-watch.component.ts
@@ -1,9 +1,12 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { WatchRecord } from 'src/app/models/watch-record';
 import { ToastService } from 'src/app/layout/service/toast.service';
 import { DateService } from 'src/app/services/date.service';
 import { WatchRecordService } from 'src/app/services/watch-record.service';
+import { FileUpload } from 'primeng/fileupload';
+import { FileService } from 'src/app/services/file.service';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-add-watch',
@@ -11,17 +14,26 @@ import { WatchRecordService } from 'src/app/services/watch-record.service';
   styleUrl: './add-watch.component.scss'
 })
 export class AddWatchComponent {
+  @ViewChild('fileUpload') fileUpload!: FileUpload;
   @Input({ required: true }) modelId?: number;
   @Output() add = new EventEmitter<WatchRecord>();
   @Output() cancel = new EventEmitter();
 
   watchForm: FormGroup;
+  fileToUpload: any;
+
+  private isLoadingSubject: BehaviorSubject<boolean>;
+  isLoading$: Observable<boolean>;
 
   constructor(
     private toastService: ToastService,
     private watchRecordService: WatchRecordService,
-    private dateService: DateService
-  ) {
+    private dateService: DateService,
+    private fileService: FileService
+  )
+  {
+    this.isLoadingSubject = new BehaviorSubject<boolean>(false);
+    this.isLoading$ = this.isLoadingSubject.asObservable();
     this.initForm();
   }
 
@@ -43,11 +55,37 @@ export class AddWatchComponent {
   }
 
   onSubmit() {
+    this.fileUpload.upload();
+    this.isLoadingSubject.next(true);
+
+    if (this.fileToUpload != null) {
+      this.fileService.uploadFile(this.fileToUpload).subscribe({
+        next: (result: any) => this.addWatch(result.uri),
+        error: (error) => {
+          this.toastService.showError("Error", error);
+          this.isLoadingSubject.next(false);
+        }
+      })
+    } else {
+      this.addWatch();
+    }
+  }
+
+  onCancel() {
+    this.watchForm.reset();
+    this.cancel.emit();
+  }
+
+  onUpload(e: any) {
+    this.fileToUpload = e.files[0];
+  }
+
+  private addWatch(imageUri?: string) {
     const formValues = this.watchForm.value;
 
     const record =
     {
-      "imageUrl": "https://lucerneluxe.com/product/rolex-collection/submariner/m126610ln-0001/",
+      "imageUrl": imageUri ?? "https://media.istockphoto.com/id/920345886/vector/classic-wristwatch-icon.jpg?s=612x612&w=0&k=20&c=MbgyLy52hlVIOqppuIpML4XSuxFUJozffHVH0Q5jMZ4=",
       "modelId": this.modelId,
       "description": formValues.description,
       "material": formValues.material,
@@ -68,20 +106,12 @@ export class AddWatchComponent {
         this.watchForm.reset();
         this.add.emit(watchRecord);
         this.toastService.showSuccess("Success!", "New watch record added");
+        this.isLoadingSubject.next(false);
       },
       error: (error) => {
         this.toastService.showError("Error", error);
+        this.isLoadingSubject.next(false);
       }
     })
-
-  }
-
-  onCancel() {
-    this.watchForm.reset();
-    this.cancel.emit();
-  }
-
-  onUpload(e: any) {
-
   }
 }
