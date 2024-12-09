c7a3f04da36c10c0e7b16b6d3b1bad36f6de27b0
James Esguerra <jamesesguerra025@gmail.com>
2024-12-09 10:39:50 +0800

    delete extra pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,6 @@ import { authGuard } from './guards/auth.guard';
                     { path: '', loadChildren: () => import('./demo/components/dashboard/dashboard.module').then(m => m.DashboardModule) },
                     { path: 'sheets', loadChildren: () => import('./pages/sheets/sheets.module').then(m => m.SheetsModule) },
                     { path: 'brands', loadChildren: () => import('./pages/brands/brands.module').then(m => m.BrandsModule) },
-                    { path: 'documentation', loadChildren: () => import('./demo/components/documentation/documentation.module').then(m => m.DocumentationModule) },
                     { path: 'blocks', loadChildren: () => import('./demo/components/primeblocks/primeblocks.module').then(m => m.PrimeBlocksModule) },
                     { path: 'pages', loadChildren: () => import('./demo/components/pages/pages.module').then(m => m.PagesModule) }
                 ],
