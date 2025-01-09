949070ae174a6bad855d95ddfacd030cbc6f7a71
James Esguerra <jamesesguerra025@gmail.com>
2025-01-09 15:02:54 +0800

    use 2 digits of precision for formatting prices

--- src/app/services/price-formatter.service.ts
+++ src/app/services/price-formatter.service.ts
@@ -6,9 +6,9 @@ import { Injectable } from '@angular/core';
 export class PriceFormatterService {
   format(value: number) : string {
     if (value >= 1_000_000_000) {
-      return `${(value / 1_000_000_000).toFixed(1).replace(/\.0$/, '')}B`;
+      return `${(value / 1_000_000_000).toFixed(2).replace(/\.0$/, '')}B`;
     } else if (value >= 1_000_000) {
-      return `${(value / 1_000_000).toFixed(1).replace(/\.0$/, '')}M`;
+      return `${(value / 1_000_000).toFixed(2).replace(/\.0$/, '')}M`;
     } else if (value >= 1_000) {
       return `${(value / 1_000).toFixed(1).replace(/\.0$/, '')}K`;
     }
